297a687ff5185447e2aed091aada30961b5d3034
densolo <programmistsolo@gmail.com>
2022-06-23 09:54:03 +0300

    собственно тут нечего сказать

---
+++ Documents/server/main.cpp
@@ -0,0 +1,20 @@
+#include <QCoreApplication>
+#include "server.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+  /*  QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "server_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }*/
+    Server s;
+
+    return a.exec();
+}
